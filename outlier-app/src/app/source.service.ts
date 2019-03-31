4d054567578d77e637f0b626c17d9849cc0d6d85
Sander <sander.vandemoortel@gmail.com>
2019-04-01 00:49:56 +0200

    Revert "add progress bar to source file upload"
    This reverts commit 48c84368e6cc1ac050f320ddd2d7804ad5bf6af8.

--- outlier-app/src/app/source.service.ts
+++ outlier-app/src/app/source.service.ts
@@ -1,13 +1,12 @@
 import { environment } from '../environments/environment';
 
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of, Subject } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Source } from './source';
 import { UserService } from './user.service';
-// import { HttpRequest } from 'selenium-webdriver/http';
 
 const sourceUrl: string = environment.apiURL + "source/";
 const awsSignatureUrl = environment.apiURL + "get_presigned_url/";
@@ -23,7 +22,7 @@ export class SourceService {
   constructor(
     private http: HttpClient,
     public userService: UserService,
-  ) { }
+   ) { }
 
   public listSources(): Observable<Source[]> {
     var httpOptions = {
@@ -38,16 +37,16 @@ export class SourceService {
       return of(this.sources);
     }
 
-    return this.http.get<Source[]>(sourceUrl, httpOptions).pipe(
-      tap(sources => {
+  	return this.http.get<Source[]>(sourceUrl, httpOptions).pipe(
+  		tap(sources => {
         console.log(`got sources`);
         sources.forEach((source, index, sources) => {
           source = new Source().deserialise(source);
           this.sources.push(source);
         });
-      }),
-      catchError(this.handleError<Source[]>('Sources'))
-    );
+  		}),
+  		catchError(this.handleError<Source[]>('Sources'))
+    	);
   }
 
   public addSource(source: Source): Observable<Source> {
@@ -74,30 +73,30 @@ export class SourceService {
     return this.http.put<Source>(sourceUrl, source, httpOptions);
   }
 
-  public deleteSource(source: Source): Observable<any> {
-    var httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': 'JWT ' + this.userService.token
-      })
-    };
+public deleteSource(source: Source): Observable<any> {
+  var httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'JWT ' + this.userService.token
+    })
+  };
 
-    return this.http.delete<Source>(sourceUrl + source.id, httpOptions).pipe(
-      tap(data => {
-        this.sources = this.sources.filter(s => s !== source);
-      })
-    );
-  }
+  return this.http.delete<Source>(sourceUrl + source.id, httpOptions).pipe(
+    tap(data => {
+      this.sources = this.sources.filter(s => s !== source);
+    })
+   );
+}
 
   public findSourceById(id: number): Source | null {
     return this.sources.find(s => s.id === id);
   }
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
-    };
+  private handleError<T> (operation = 'operation', result?: T) {
+  	return (error: any): Observable<T> => {
+  		console.log(`${operation} failed: ${error.message}`);
+  		return of(result as T);
+  	};
   }
 
   public uploadCSV(csvFile: File, source: Source): Observable<any> {
@@ -132,11 +131,10 @@ export class SourceService {
   }
 
   public uploadToS3(file: File, presignedURL: string): Observable<any> {
-    const req = new HttpRequest('PUT', presignedURL, file, {
-      headers: new HttpHeaders({ 'Content-Type': file.type }),
-      reportProgress: true,
-    });
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': file.type})
+    }
 
-    return this.http.request(req);
+    return this.http.put(presignedURL, file, httpOptions);
   }
 }
